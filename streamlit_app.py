09e5ce9fbe83d5cf9e39d5f64cc3ab96b7e9b90d
Adit080 <adityaharun1140@gmail.com>
2025-05-27 11:32:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,9 @@ st.write("Foto tersebut adalah foto pelaku maling di daerah gasibu, dia mengambi
 
 st.image("IMG-20250524-WA0118.jpg", width=200) 
 
-st.title(" Pelaku Bom Bunuh Diri") 
+st.title("Pelaku Bom Bunuh Diri") 
 st.write("motif bom bunuh diri belum diketahuinya oleh polisi setempat, namun menurut keluarga pelaku tersebut sakit hati karena diputusin oleh kekasihnya sendiri") 
 
+st.title("Korban Bullying") 
+st.write("Foto di atas adalah korban bullying di SMAN 00 Monokotobo, menurut teman kelasnya dia sudah menjadi korban bullying selama 2 Tahun, dia di bully oleh temannya yang berinisial R dan N. Sekarang korban masih trauma dan tidak mau sekolah) 
 st.image("IMG-20250524-WA0069.jpg", width=200) 
