c6dd39d8e2cb12bfa0d5032d506670019602afe5
Adit080 <adityaharun1140@gmail.com>
2025-05-27 11:04:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,9 @@ st.title("Selamat Datang di Web Adit")
 st.write(
     "ngodingseru bersama Aditya Harun"
 )
+st.write(
+    "Pelaku Pelecehan terhadap perempuan di bawah umur"
 st.image("WhatsApp Image 2025-05-26 at 20.48.22.jpeg", width=200)
+st.write(
+    "Buronan!!!"
+st. image("IMG-20250524-WA0118.jpg", width=200) 
