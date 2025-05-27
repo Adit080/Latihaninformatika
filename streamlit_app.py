1296fac952dcdac2f3439ff09e47a2628f6f4318
Adit080 <adityaharun1140@gmail.com>
2025-05-27 11:20:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,9 @@ st.title("Saksikanlah Pertandingan kkjahe vs aziz gagak")
 st.image("WhatsApp Image 2025-05-26 at 20.48.22.jpeg", width=200)
 
 st.title("Buronan!!!") 
+st.write("Foto tersebut adalah foto pelaku maling di daerah gasibu, dia mengambil sebuah dompet di daerah gasibu di saat ibu itu sedang mengeluarkan HP") 
 st.image("IMG-20250524-WA0118.jpg", width=200) 
 
-st.title(" Pelaku Pelecehan kepada anak di bawah umur") 
+st.title(" Pelaku Bom Bunuh Diri") 
+st.write("motif bom bunuh diri belum diketahuinya oleh polisi setempat, namun menurut keluarga pelaku tersebut sakit hati karena diputusin oleh kekasihnya sendiri") 
 st.image("IMG-20250524-WA0069.jpg", width=200) 
